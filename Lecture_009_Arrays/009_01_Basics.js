3631a2974e03103c241b3ec3477a017876590a10
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-08 19:36:22 +0530

    009_01_Basics

---
+++ Lecture_009_Arrays/009_01_Basics.js
@@ -0,0 +1,49 @@
+// declaring the array: 
+
+let arr = [2,4,6,8]
+
+console.log(arr)
+console.log(arr[2])
+// -------------------------
+
+
+// accessing the values
+console.log(arr[2])
+
+// Output:
+// [ 2, 4, 6, 8 ]
+// 6
+
+
+// ---------------------------
+
+//printing the array
+let arr2 = [2,4,6,8]
+console.log(arr2)
+
+// Output:
+// [ 2, 4, 6, 8 ]
+
+
+//-----------------------
+
+
+// Array implimentation: 
+
+let arrayy = [2,4,6,8]
+
+console.log(arrayy[2])
+console.log(arrayy[3])
+
+for(let i=0; i<=arrayy.length-1; i++){
+    console.log(arrayy[i])
+}
+
+// Output:
+// 6
+// 8
+
+// 2
+// 4
+// 6
+// 8
